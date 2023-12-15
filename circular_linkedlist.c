fb220128639db4bbb31d6631a5e1333a965d2fac
solanki jignesh <123371776+Jignesh4611@users.noreply.github.com>
2023-12-15 09:43:58 +0530

    circulae_linkedlist

--- circular_linkedlist.c
+++ circular_linkedlist.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
-struct node 
+#include <stdio.h>
+struct node
 {
     int data;
     struct node *next;
-};
+} *head, *last,*t;
+void create(int A[], int n)
+{
+    int i;
+    head = (struct node *)malloc(sizeof(struct node));
+    head->data = A[0];
+    head->next = head;
+    last = head;
+
+for (i = 1; i < n; i++)
+{
+    t=(struct node *)malloc(sizeof(struct node));
+    t->data=A[i];
+    t->next= last->next;
+    last->next=t;
+    last=t;
+}
+}
