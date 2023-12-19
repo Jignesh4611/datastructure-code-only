d8f8c823d6ad7e044e0a7282126dc46efc4409dc
solanki jignesh <123371776+Jignesh4611@users.noreply.github.com>
2023-12-19 12:33:04 +0530

    for delete a value

--- circular_linkedlist.c
+++ circular_linkedlist.c
@@ -9,13 +9,17 @@ void create(int A[], int n);
 int length(struct node *p);
 void Insert(struct node *p, int index, int x);
 void display(struct node *h);
+int delete(struct node *p, int index);
 
 int main()
 {
-    int A[] = {1,2, 3, 4, 5, 6};
+    int A[] = {1, 2, 3, 4, 5, 6};
     create(A, 6);
-    Insert(head, 3, 10);
+
     display(head);
+    delete (head, 2);
+    display(head);
+    display(last);
 }
 
 void create(int A[], int n)
@@ -39,14 +43,14 @@ void create(int A[], int n)
 int length(struct node *p)
 {
     int x = 0;
-   do {
+    do
+    {
         x++;
         p = p->next;
-    }
-     while (p != head);
-    return x ;
+    } while (p != head);
+    return x;
 }
-//before that position
+// before that position
 void Insert(struct node *p, int index, int x)
 {
     struct node *t;
@@ -64,8 +68,7 @@ void Insert(struct node *p, int index, int x)
         {
             head = t;
             head->next = head;
-            last=head;
-            
+            last = head;
         }
         else
         {
@@ -75,24 +78,20 @@ void Insert(struct node *p, int index, int x)
             }
             p->next = t;
             t->next = head;
-            head=t;
-            
-            
+            head = t;
         }
     }
     else
     {
-        for (i =1 ; i < index-2 ; i++)
+        for (i = 1; i < index - 2; i++)
         {
-                  p=p->next;
+            p = p->next;
         }
-            t = (struct node *)malloc(sizeof(struct node));
-            t->data = x;
-            t->next = p->next;
-            p->next = t; 
-            last=t;
-
-
+        t = (struct node *)malloc(sizeof(struct node));
+        t->data = x;
+        t->next = p->next;
+        p->next = t;
+        last = t;
     }
 }
 
@@ -108,8 +107,49 @@ void display(struct node *h)
         {
             printf("%d ", h->data);
             h = h->next;
-        } 
-        while (h != head);
-        
-    }printf("\n");
+        } while (h != head);
+    }
+    printf("\n");
+}
+int delete(struct node *p, int index)
+{
+    struct node *q;
+    int i, x;
+    if (index < 0 || index > length(head))
+    {
+        return -1;
+    }
+
+    if (index == 1)
+    {
+        while (p->next != head)
+        {
+            p = p->next;
+        }
+        x = head->data;
+        if (head == p)
+        {
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            p->next = head->next;
+            free(head);
+            head = p->next;
+        }
+    }
+    else
+    {
+
+        for (i = 0; i < index - 2; i++)
+        {
+            p = p->next;
+        }
+        q = p->next;
+        p->next = q->next;
+        x = q->data;
+        free(q);
+    }
+    return x;
 }
