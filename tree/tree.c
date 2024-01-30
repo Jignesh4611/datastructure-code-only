906759436f2f8c8f0d98cfc3591610a5ade29488
solanki jignesh <123371776+Jignesh4611@users.noreply.github.com>
2024-01-30 16:02:19 +0530

    nothing

--- tree/tree.c
+++ tree/tree.c
@@ -1,52 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "tree.h"
-#include<stdlib.h>
-struct node *root=NULL;
+#include <stdlib.h>
+struct node *root = NULL;
 void treecreate()
 {
-    struct node *p,*t;
-    int x; 
+    struct node *p, *t;
+    int x;
     struct queue q;
-    create(&q,100);
-    printf("enter root value ");
-    scanf("%d",&x);
-    root=(struct node *)malloc(sizeof(struct node ));
-    root->data=x;
-    root->lchild=root->rchild=NULL;
-    enqueue(&q,root);
+    create(&q, 100);
+    printf("enter root value :");
+    scanf("%d", &x);
+    root = (struct node *)malloc(sizeof(struct node));
+    root->data = x;
+    root->lchild = root->rchild = NULL;
+    enqueue(&q, root);
     while (!isempty(q))
     {
-        p=dequeue(&q);
+        p = dequeue(&q);
         printf("enter left child %d:", p->data);
-        scanf("%d",&x);
-        if(x!=-1)
+        scanf("%d", &x);
+        if (x != -1)
         {
-            
-            t=(struct node *)malloc(sizeof(struct node ));
-            t->data=x;
-            t->lchild=t->rchild=NULL;
-            p->lchild=t;
-            enqueue(&q,t);
+
+            t = (struct node *)malloc(sizeof(struct node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->lchild = t;
+            enqueue(&q, t);
         }
-        printf("enter right child %d:",p->data);
-        scanf("%d",&x);
-        if(x!=-1)
+        printf("enter right child %d:", p->data);
+        scanf("%d", &x);
+        if (x != -1)
         {
-            
-            t=(struct node *)malloc(sizeof(struct node ));
-            t->data=x;
-            t->lchild=t->rchild=NULL;
-            p->rchild=t;
-            enqueue(&q,t);
- 
+
+            t = (struct node *)malloc(sizeof(struct node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->rchild = t;
+            enqueue(&q, t);
         }
-    }    
+    }
 }
 void preorder(struct node *p)
 {
-    if(p)
+    if (p)
     {
-        printf(" %d ",p->data);
+        printf(" %d ", p->data);
         preorder(p->lchild);
         preorder(p->rchild);
     }
